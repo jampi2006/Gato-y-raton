5957e81c9ce6f29d348697bb6efacbb91f9c7f6c
jampi2006 <jamphierorellana641@gmail.com>
2024-05-17 15:50:49 -0600

    guardado de respaldo 2

---
+++ js/IA.js
@@ -0,0 +1,50 @@
+//Creamos una matriz para guiarnos con el proceso y entender el juego y las pocisiones
+let matriz = [
+    ["","",""],
+    ["","",""],
+    ["","",""]
+
+]
+
+//Creamos una variable que trae todos los DIVs del HTML
+//getElementsByClassName toma todos los divs con la misma clase
+let posicion = document.getElementsByClassName("posicion")
+
+//creamos una funcion que va a recibir 3 parametros
+//los 2 primeros parametros son la posicion de la matriz
+//y el tercer parametro "Index" es la posicion del div en el HTML
+function fun1(num1, num2, index) {
+    console.log(num1 , num2);
+
+    //aqui marcamos en la casilla del HTML
+    posicion[index].innerHTML = "O"
+
+    //Aqui marcamos en la matriz
+    matriz[num1][num2]= "X"
+
+    
+    //aqui imprimimos la matriz en consola
+    console.log(matriz);
+}
+
+function fun3(num3, num4, index) {
+    console.log(num3 , num4);
+
+    posicion[index].innerHTML = "O"
+
+    matriz[num3][num4]= "X"
+
+    console.log(matriz);
+}
+
+function fun4(num5, num6, index) {
+    console.log(num5 , num6);
+
+    posicion[index].innerHTML= "O"
+
+    matriz[num5][num5]= "X"
+
+    console.log(matriz);
+
+}
+
---
+++ js/inicio.js
@@ -0,0 +1,167 @@
+let cuadro1 = document.getElementById("cuadros1")
+let cuadro2 = document.getElementById("cuadros2")
+let cuadro3 = document.getElementById("cuadros3")
+let cuadro4 = document.getElementById("cuadros4")
+let cuadro5 = document.getElementById("cuadros5")
+let cuadro6 = document.getElementById("cuadros6")
+let cuadro7 = document.getElementById("cuadros7")
+let cuadro8 = document.getElementById("cuadros8")
+let cuadro9 = document.getElementById("cuadros9")
+// Primero declaro las varibles
+
+let chichi = true
+
+// let matriz = [
+//     [cuadro1,cuadro2,cuadro3],
+//     [cuadro4,cuadro5,cuadro6],
+//     [cuadro7,cuadro8,cuadro9],
+// ]
+
+
+let rato1 = true
+
+function fun1() {
+    if (rato1 === true) {
+        if (chichi === true) {
+            cuadro1.innerHTML = 'X'
+            chichi = false;
+        } else {
+            cuadro1.innerHTML = 'O'
+            chichi = true;
+        }
+        rato1 = false;
+    } else {
+        console.log('game over');
+    }
+}
+console.log(chichi);
+
+let rato2 = true
+
+function fun2() {
+    if (rato2 === true) {
+        if (chichi === true) {
+            cuadro2.innerHTML = 'X'
+            chichi = false;
+        } else {
+            cuadro2.innerHTML = 'O'
+            chichi = true;
+        }
+        rato2 = false;
+    } else {
+        console.log('game over');
+    }
+}
+
+let rato3 = true
+
+function fun3() {
+    if (rato3 === true) {
+        if (chichi === true) {
+            cuadro3.innerHTML = 'X'
+            chichi = false;
+        } else {
+            cuadro2.innerHTML = 'O'
+            chichi = true;
+        }
+        rato3 = false;
+    } else {
+        console.log('game over');
+    }
+}
+
+let rato4 = true
+function fun4() {
+    if (rato4 === true) {
+        if (chichi === true) {
+            cuadro4.innerHTML = 'X'
+            chichi = false;
+        } else {
+            cuadro4.innerHTML = 'O'
+            chichi = true;
+        }
+        rato4 = false;
+    } else {
+        console.log('game over');
+    }
+}
+
+let rato5 = true
+function fun5() {
+    if (rato5 === true) {
+        if (chichi === true) {
+            cuadro5.innerHTML = 'X'
+            chichi = false;
+        } else {
+            cuadro5.innerHTML = 'O'
+            chichi = true;
+        }
+        rato5 = false;
+    } else {
+        console.log('game over');
+    }
+}
+
+let rato6 = true
+function fun6() {
+    if (rato6 === true) {
+        if (chichi === true) {
+            cuadro6.innerHTML = 'X'
+            chichi = false;
+        } else {
+            cuadro6.innerHTML = 'O'
+            chichi = true;
+        }
+        rato6 = false;
+    } else {
+        console.log('game over');
+    }
+}
+
+let rato7 = true
+function fun7() {
+    if (rato7 === true) {
+        if (chichi === true) {
+            cuadro7.innerHTML = 'X'
+            chichi = false;
+        } else {
+            cuadro7.innerHTML = 'O'
+            chichi = true;
+        }
+        rato7 = false;
+    } else {
+        console.log('game over');
+    }
+}
+
+let rato8 = true
+function fun8() {
+    if (rato8 === true) {
+        if (chichi === true) {
+            cuadro8.innerHTML = 'X'
+            chichi = false;
+        } else {
+            cuadro8.innerHTML = 'O'
+            chichi = true;
+        }
+        rato8 = false;
+    } else {
+        console.log('game over');
+    }
+}
+
+let rato9 = true
+function fun9() {
+    if (rato9 === true) {
+        if (chichi === true) {
+            cuadro9.innerHTML = 'X'
+            chichi = false;
+        } else {
+            cuadro9.innerHTML = 'O'
+            chichi = true;
+        }
+        rato9 = false;
+    } else {
+        console.log('game over');
+    }
+}
