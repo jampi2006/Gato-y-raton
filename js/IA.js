30502c3aa1cba4282523a1ffe55012fd861c3843
jampi2006 <jamphierorellana641@gmail.com>
2024-05-20 16:00:37 -0600

    cree funciones de balidar el gane

--- js/IA.js
+++ js/IA.js
@@ -1,8 +1,8 @@
 //Creamos una matriz para guiarnos con el proceso y entender el juego y las pocisiones
 let matriz = [
-    ["","",""],
-    ["","",""],
-    ["","",""]
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
 
 ]
 
@@ -14,37 +14,48 @@ let posicion = document.getElementsByClassName("posicion")
 //los 2 primeros parametros son la posicion de la matriz
 //y el tercer parametro "Index" es la posicion del div en el HTML
 function fun1(num1, num2, index) {
-    console.log(num1 , num2);
+    console.log(num1, num2);
 
     //aqui marcamos en la casilla del HTML
-    posicion[index].innerHTML = "O"
+    posicion[index].innerHTML = "X"
 
     //Aqui marcamos en la matriz
-    matriz[num1][num2]= "X"
+    matriz[num1][num2] = "X"
 
-    
+
     //aqui imprimimos la matriz en consola
     console.log(matriz);
 }
 
 function fun3(num3, num4, index) {
-    console.log(num3 , num4);
+    console.log(num3, num4);
 
-    posicion[index].innerHTML = "O"
+    posicion[index].innerHTML = "X"
 
-    matriz[num3][num4]= "X"
+    matriz[num3][num4] = "X"
 
     console.log(matriz);
 }
 
 function fun4(num5, num6, index) {
-    console.log(num5 , num6);
+    console.log(num5, num6);
 
-    posicion[index].innerHTML= "O"
+    posicion[index].innerHTML = "X"
 
-    matriz[num5][num5]= "X"
+    matriz[num5][num6] = 'X'
 
     console.log(matriz);
-
 }
 
+
+
+
+
+
+
+
+// math random
+
+let stilos = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+console.log(Math.floor(Math.random(stilos) * stilos.length));
--- js/inicio.js
+++ js/inicio.js
@@ -9,41 +9,46 @@ let cuadro8 = document.getElementById("cuadros8")
 let cuadro9 = document.getElementById("cuadros9")
 // Primero declaro las varibles
 
-let chichi = true
 
-// let matriz = [
-//     [cuadro1,cuadro2,cuadro3],
-//     [cuadro4,cuadro5,cuadro6],
-//     [cuadro7,cuadro8,cuadro9],
-// ]
+//Creamos una matriz para guiarnos con el proceso y entender el juego y las pocisiones
+let matriz = [
+    ["","",""],
+    ["","",""],
+    ["","",""],
+]
 
+let rato1 = true//se da la iniciación para cuando sea verdadero marque "X" si no una "O"
+let chichi = true//esta variable es como el cerebro que hace que funcione porque si no marca la "X" o "O" dependiendo si es falso o verdadero
 
-let rato1 = true
-
-function fun1() {
+function fun1(i,j) {
     if (rato1 === true) {
         if (chichi === true) {
-            cuadro1.innerHTML = 'X'
-            chichi = false;
+             matriz [0][0] = "X"//aqui se le dan las cordenadas para que ponga la "X" en la casilla indicada pero solo se muestra en la matriz
+            cuadro1.innerHTML = 'X'//aqui muestra la "X" en pantalla
+            chichi = false;//si la varible chichi es falso pondra una "O" en lugar de una "X"
         } else {
+             matriz [0][0] = "O"
             cuadro1.innerHTML = 'O'
             chichi = true;
         }
-        rato1 = false;
+        rato1 = false;//aqui la funcion se pone en falso para desactivarla porque al inicio se activa con el verdadero
     } else {
-        console.log('game over');
+        console.log('game over');//aqui se muestra en consola el mensaje si intentas poner el simbolo en una casilla que ya esta ocupada es como una alerta que dice ya esta ocupado este campo
     }
 }
-console.log(chichi);
+
+// console.log(chichi);
 
 let rato2 = true
 
 function fun2() {
     if (rato2 === true) {
         if (chichi === true) {
+            matriz [0][1] = "X"
             cuadro2.innerHTML = 'X'
             chichi = false;
         } else {
+            matriz [0][1] = "O"
             cuadro2.innerHTML = 'O'
             chichi = true;
         }
@@ -58,9 +63,11 @@ let rato3 = true
 function fun3() {
     if (rato3 === true) {
         if (chichi === true) {
+            matriz [0][2] = "X"
             cuadro3.innerHTML = 'X'
             chichi = false;
         } else {
+            matriz [0][2] = "O"
             cuadro2.innerHTML = 'O'
             chichi = true;
         }
@@ -74,9 +81,11 @@ let rato4 = true
 function fun4() {
     if (rato4 === true) {
         if (chichi === true) {
+            matriz [1][0] = "X"
             cuadro4.innerHTML = 'X'
             chichi = false;
         } else {
+            matriz [1][0] = "O"
             cuadro4.innerHTML = 'O'
             chichi = true;
         }
@@ -90,9 +99,11 @@ let rato5 = true
 function fun5() {
     if (rato5 === true) {
         if (chichi === true) {
+            matriz [1][1] = "X"
             cuadro5.innerHTML = 'X'
             chichi = false;
         } else {
+            matriz [1][1] = "O"
             cuadro5.innerHTML = 'O'
             chichi = true;
         }
@@ -106,9 +117,11 @@ let rato6 = true
 function fun6() {
     if (rato6 === true) {
         if (chichi === true) {
+            matriz [1][2] = "X"
             cuadro6.innerHTML = 'X'
             chichi = false;
         } else {
+            matriz [1][2] = "O"
             cuadro6.innerHTML = 'O'
             chichi = true;
         }
@@ -122,9 +135,11 @@ let rato7 = true
 function fun7() {
     if (rato7 === true) {
         if (chichi === true) {
+            matriz [2][0] = "X"
             cuadro7.innerHTML = 'X'
             chichi = false;
         } else {
+            matriz [2][0] = "O"
             cuadro7.innerHTML = 'O'
             chichi = true;
         }
@@ -138,9 +153,11 @@ let rato8 = true
 function fun8() {
     if (rato8 === true) {
         if (chichi === true) {
+            matriz [2][1] = "X"
             cuadro8.innerHTML = 'X'
             chichi = false;
         } else {
+            matriz [2][1] = "O"
             cuadro8.innerHTML = 'O'
             chichi = true;
         }
@@ -154,14 +171,36 @@ let rato9 = true
 function fun9() {
     if (rato9 === true) {
         if (chichi === true) {
+            matriz [2][2] = "X"
             cuadro9.innerHTML = 'X'
             chichi = false;
         } else {
+            matriz [2][2] = "O"
             cuadro9.innerHTML = 'O'
             chichi = true;
         }
         rato9 = false;
     } else {
         console.log('game over');
+    }
+}
+///////////////////////////////////////////// parte de validación////////////////////////////////////////////////////////////////////
+
+function validar() {
+
+    let contadorX = 0;// declaro una vaiable que contara las "X"
+    let contadorO = 0;// declaro otra vaiable que contara las "O"
+
+    for (let index = 0; index < array.length; index++) {// creo un for que va a seguir la primera linea del [0]
+        if (matriz[0][index] == "X") {
+            contadorX++; // este es el contador
+        } else {(matriz[0][index] == "O") 
+        contadorO++;
+        }
+    }
+    if (contadorX == 3 || contadorO == 3) {// aqui se ve si la primera linea esta llena de "X" o "O"  si se cumple se vuelve true si no se vuelve false 
+        return true
+    } else {
+        return false    
     }
 }
